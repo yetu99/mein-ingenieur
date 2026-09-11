96c78bb2099e36091228d5b510b4470c6a7cf72d
Seti <ladegeraet_garn.6d@icloud.com>
2026-09-11 22:41:40 +0200

    chore(app): gitignore und eslint-ignores ergaenzen

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import prettier from 'eslint-config-prettier'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'coverage'] },
   {
     files: ['**/*.{ts,tsx}'],
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
